3a5b8b29b8a63a76e65bd139187d7cdc6bf46be2
connorwillchris <connorwillchris@gmail.com>
2024-03-07 11:31:30 -0700

    first commit

---
+++ cavestory/CaveStory.cs
@@ -0,0 +1,11 @@
+﻿using Vintagestory.API.Common;
+
+namespace cavestory;
+
+public class CaveStory : ModSystem {
+    
+    public override void Start(ICoreAPI api) {
+        api.Logger.Notification("Hello world from cavestory");
+        
+    }
+}
