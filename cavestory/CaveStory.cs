76170930b9da975e3b3dff35ad437feaa65a754e
connorwillchris <connorwillchris@gmail.com>
2024-03-08 11:15:39 -0700

    updates to the dimo-pachy

--- cavestory/CaveStory.cs
+++ cavestory/CaveStory.cs
@@ -1,22 +1,19 @@
 ﻿//using System.Reflection.Metadata.Ecma335;
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Server;
 
 namespace cavestory;
 
 public class CaveStory : ModSystem {
-    
     private ICoreServerAPI serverApi;
+    private ICoreClientAPI clientAPI;
 
     public override void Start(ICoreAPI api) {
         api.Logger.Notification("Hello world from cavestory");
     }
 
     public override void StartServerSide(ICoreServerAPI api) {
-        this.serverApi = api;
-    }
-
-    public override bool ShouldLoad(EnumAppSide side) {
-        return side == EnumAppSide.Server;
+        serverApi = api;
     }
 }
