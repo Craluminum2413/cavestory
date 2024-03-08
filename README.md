722f8e5b2adfd12b63561a60433e35ee754df9dc
connorwillchris <connorwillchris@gmail.com>
2024-03-08 09:07:49 -0700

    small updates

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cave Story - Vintage Story Mod
+
+version 0.1.0-alpha
+
+- pachycephalosaurus
--- cavestory/CaveStory.cs
+++ cavestory/CaveStory.cs
@@ -1,11 +1,22 @@
-﻿using Vintagestory.API.Common;
+﻿//using System.Reflection.Metadata.Ecma335;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
 
 namespace cavestory;
 
 public class CaveStory : ModSystem {
     
+    private ICoreServerAPI serverApi;
+
     public override void Start(ICoreAPI api) {
         api.Logger.Notification("Hello world from cavestory");
-        
+    }
+
+    public override void StartServerSide(ICoreServerAPI api) {
+        this.serverApi = api;
+    }
+
+    public override bool ShouldLoad(EnumAppSide side) {
+        return side == EnumAppSide.Server;
     }
 }
